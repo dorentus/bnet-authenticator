3b5de5785a637f5df2906a94854065ee649c4914
Zhang Yi <zhangyi.cn@gmail.com>
2014-02-21 20:57:49 +0800

    cleanup tests

--- test/test_battlenet_authenticator.rb
+++ test/test_battlenet_authenticator.rb
@@ -12,25 +12,6 @@ class Bna::AuthenticatorTest < Test::Unit::TestCase
     is_default_authenticator authenticator
   end
 
-  def test_request_new_serial
-    authenticator = Bna::Authenticator.new(:region => :US)
-    assert_equal :US, authenticator.region
-    assert_not_nil authenticator.serial
-    assert_not_nil authenticator.secret
-    assert_not_nil authenticator.restorecode
-  end
-
-  def test_restore
-    authenticator = Bna::Authenticator.new(:serial => DEFAULT_SERIAL, :restorecode => DEFAULT_RSCODE)
-    is_default_authenticator authenticator
-  end
-
-  def test_request_server_time
-    assert_nothing_raised do
-      Bna::Authenticator.request_server_time :EU
-    end
-  end
-
   private
 
   def is_default_authenticator(authenticator)
